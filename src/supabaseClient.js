97a484702d1c9453e40bbf4e3582b0f68b76d429
CloudyRaowdy <nischayachamlingrai@gmail.com>
2025-10-29 14:07:36 +0545

    added supa base

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Find these in your Supabase dashboard under Project Settings → API
-const supabaseUrl = 'https://yncdcmjwszvrympeqybj.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InluY2RjbWp3c3p2cnltcGVxeWJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE1NDE5MzgsImV4cCI6MjA3NzExNzkzOH0.MIrESAplLbLVjjniItN-YrvOkduRXmx_Jpesna68qic';
-
+const supabaseUrl = 'https://lkdcycmxcyxpkzfrjzdg.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxrZGN5Y214Y3l4cGt6ZnJqemRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2NTQyNzIsImV4cCI6MjA3NzIzMDI3Mn0.1xk7qrLCtjJoWHb8PAacbOh0b8nS7RhbIPh2xs9azxY';
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
